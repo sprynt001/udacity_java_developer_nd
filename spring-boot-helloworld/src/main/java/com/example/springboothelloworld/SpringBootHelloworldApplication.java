7b8bf5fe8f87190d82a907e6988823f1f12cd747
kola <kola@sprynt.com>
2019-10-22 10:44:32 +0100

    Working version of simple controller implementation

---
+++ spring-boot-helloworld/src/main/java/com/example/springboothelloworld/SpringBootHelloworldApplication.java
@@ -0,0 +1,14 @@
+package com.example.springboothelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringBootHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHelloworldApplication.class, args);
+    }
+
+}
--- spring-boot-helloworld/src/main/java/com/example/springboothelloworld/controller/HelloController.java
+++ spring-boot-helloworld/src/main/java/com/example/springboothelloworld/controller/HelloController.java
@@ -1,21 +1,20 @@
-package controller;
+package com.example.springboothelloworld.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Controller
 public class HelloController {
     private Map<String, Object> result = new HashMap<>();
 
     @RequestMapping("/hello")
     @ResponseBody
     public Map<String, Object> hello() {
-        this.result.put("name", "Stephen");
-        this.result.put("city", "San Jose");
-        return this.result;
-
-
+        result.put("name", "Stephen");
+        return result;
     }
 }
